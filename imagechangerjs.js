eaa33c071d8575d980607e215cdd69d93ebcfc8e
moozygoo <39626225+moozygoo@users.noreply.github.com>
2019-05-24 12:52:14 +0200

    Update imagechangerjs.js

--- imagechangerjs.js
+++ imagechangerjs.js
@@ -1,8 +1,3 @@
 function imgchng(){
-        
-		//var kephelyzet=document.getElementById(picnum);
-		//if(kephelyzet=1)
-		//{
-			document.getElementById('hazkep').src='http://passzivhaz.info.hu/wp-content/uploads/2017/11/passzivhaz_nyiltnap_02.png'
-		//}
+        document.getElementById('hazkep').src='http://passzivhaz.info.hu/wp-content/uploads/2017/11/passzivhaz_nyiltnap_02.png'
     }
